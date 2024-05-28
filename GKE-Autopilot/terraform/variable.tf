79c050568e6610f60c351b7a81029c6a3bf99bd6
adminturneddevops <mlevan1992@gmail.com>
2024-05-28 08:59:11 -0400

    GKE autopilot

---
+++ GKE-Autopilot/terraform/variable.tf
@@ -0,0 +1,24 @@
+variable "project_id" {
+  type = string
+  default = "gold-mode-297211"
+}
+
+variable "region" {
+  type = string
+  default = "us-east1"
+}
+
+variable "vpc_name" {
+  type = string
+  default = "default"
+}
+
+variable "subnet_name" {
+  type = string
+  default = "default"
+}
+
+variable "cluster_name" {
+  type = string
+  default = "gkek8senv"
+}
